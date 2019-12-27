e06ffbb43875384946faf748e8184fbd82cf6e8a
Stephanie <stephanie.holcomb@gmail.com>
2019-12-27 10:20:16 -0800

    start day 4

---
+++ Day04/AoC_day04.py
@@ -0,0 +1,34 @@
+## Advent of Code 
+# --- Day 3: Crossed Wires ---
+
+# --- Part ONE ---
+
+
+# --- Part TWO ---
+
+
+
+import math
+import copy
+
+def getdata(filename, listname):
+
+	file = open(filename,'r')
+	for line in file.readlines():
+		line = line.split(',')
+		for value in line:
+			value = int(value)
+			listname.append(value)
+
+
+
+	#return data
+
+
+def main():
+
+	
+
+
+if __name__ == '__main__':
+	main()
